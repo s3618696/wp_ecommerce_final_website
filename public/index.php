5897d5bed4bdddc351e30b4065971f3634129d1d
s3618696 <s3618696@rmit.edu.vn>
2020-05-31 02:40:04 +0700

    [REMOVE] remove design files

--- public/index.php
+++ public/index.php
@@ -5,10 +5,6 @@
     <title>SweetPick</title>
 
     <meta charset="utf-8" />
-
-    <!--[if IE
-      ]><meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"
-    /><![endif]-->
     <meta
       name="viewport"
       content="width=device-width, initial-scale=1, maximum-scale=1"
